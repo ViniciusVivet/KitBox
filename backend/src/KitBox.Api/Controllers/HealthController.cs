5de9327d1487ef12c007f83841a0c31cacc73c73
Douglas Vinicius Alves da Silva <douglasvivet@gmail.com>
2025-08-24 22:59:37 -0300

    chore(api): fix HealthController quotes/route

---
+++ backend/src/KitBox.Api/Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitBox.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok(new { status = "ok", utc = DateTime.UtcNow });
+}
